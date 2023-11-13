3bd44b57afa4495e457d147842294c16ebb8d0a1
Vza <35027648+voza95@users.noreply.github.com>
2023-11-13 22:27:26 +0530

    Setup basic resources

--- app/src/main/java/com/example/myweatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myweatherapp/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myweatherapp
+package com.example.myweatherapp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myweatherapp.ui.theme.MyWeatherAppTheme
+import com.example.myweatherapp.presentation.ui.theme.MyWeatherAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
